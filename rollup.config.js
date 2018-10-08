f46a9cb69b197c8b545694f6533341596b95ac79
Christian <csvenke@gmail.com>
2018-10-08 22:10:41 +0200

    build: improve build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,62 @@
-import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import { terser } from 'rollup-plugin-terser';
+import replace from 'rollup-plugin-replace';
+import typescript from 'rollup-plugin-typescript2';
+
 import pkg from './package.json';
+
+const getExternals = obj => [
+  ...Object.keys(obj.peerDependencies),
+  ...Object.keys(obj.dependencies),
+];
 
 const createConfig = ({ plugins, ...restConfig } = {}) => ({
   input: 'src/index.ts',
-  external: [...Object.keys(pkg.peerDependencies), ...Object.keys(pkg.dependencies)],
+  external: getExternals(pkg),
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
     typescript({
       tsconfig: './tsconfig.json',
       typescript: require('typescript'),
+      rollupCommonJSResolveHack: true,
+      clean: true,
     }),
     ...plugins,
   ],
   ...restConfig,
 });
 
+const createOutput = ({ ...config }) => ({
+  exports: 'named',
+  sourcemap: true,
+  ...config,
+});
+
 export default [
   createConfig({
-    output: { file: `lib/${pkg.main}`, format: 'cjs' },
-    plugins: [sizeSnapshot(), terser()],
-  }),
-  createConfig({
-    output: { file: `lib/${pkg.module}`, format: 'esm' },
+    output: createOutput({
+      file: `lib/${pkg.module}`,
+      format: 'esm',
+    }),
     plugins: [sizeSnapshot(), terser()],
   }),
   createConfig({
     experimentalCodeSplitting: true,
     optimizeChunks: true,
     input: [
+      './src/index.ts',
       './src/components/List/List.tsx',
       './src/components/Show/Show.tsx',
       './src/components/ShowIfElse/ShowIfElse.tsx',
       './src/components/Switch/Switch.tsx',
       './src/hocs/Hideable/Hideable.tsx',
     ],
-    output: {
+    output: createOutput({
       dir: 'lib',
       format: 'cjs',
-    },
+    }),
     plugins: [terser()],
   }),
 ];
