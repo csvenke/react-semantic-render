cbad9bf94d6c4a086e92316ddb0943d24a4ae04c
Christian Svenkerud <csvenke@gmail.com>
2020-04-07 17:27:20 +0200

    fix: add semantic-release git plugin

--- release.config.js
+++ release.config.js
@@ -3,6 +3,14 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", { pkgRoot: "dist" }],
+    [
+      "@semantic-release/git",
+      {
+        assets: false,
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
     "@semantic-release/github",
   ],
 };
