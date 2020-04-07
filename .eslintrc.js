35f298be4d52e043d484ae3cf80dfaa3ef2f75ae
Christian Svenkerud <csvenke@gmail.com>
2020-04-07 15:09:48 +0200

    chore: remove unesesarry eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['standard', 'standard-react'],
-  globals: {
-    describe: true,
-    test: true,
-    expect: true
-  }
+  extends: ['standard', 'standard-react']
 }
