1665065fd75853ce178e86faa876f533c151f584
Christian Svenkerud <csvenke@gmail.com>
2020-04-07 14:52:28 +0200

    ci: testing gitlab actions

---
+++ release.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', { pkgRoot: 'dist' }],
+    '@semantic-release/github'
+  ]
+}
